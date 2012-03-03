5c26aa92cc4f1923340fcc6a8739e4ab499a0a51
Philip Sampaio <philip.sampaio@gmail.com>
2012-03-02 21:46:07 -0300

    Primeira aula

---
+++ aula1.rb
@@ -0,0 +1,63 @@
+# isso é um comentário
+
+# ============ Puts
+# puts 'Hello World!'
+#
+# puts "String é tudo que está entre aspas(simples ou duplas)"
+#
+# puts "Puts é o mesmo que print com uma quebra de linha!"
+#
+# puts 2 + 2
+#
+# puts 'Teste de concatenacao com strings' + ' está ok'
+#
+# puts "Teste de concatenacao com numeros e strings #{42 + 42}"
+# ============= Variáveis
+
+# puts "Digite seu nome"
+#
+# meu_nome = gets
+#
+# puts "Meu nome eh " + meu_nome
+#
+# puts "Digite um numero:"
+#
+# # convertendo entrada. de string para inteiro: 
+#
+# numero = gets.to_i
+#
+# puts numero + 2
+
+
+# ============== Tipos de variaveis
+
+# Para testar, vc pode usar o irb(digite isso na linha de comando)
+# que eh o interpretador interativo do Ruby
+
+# Inteiros 
+
+n1 = 10
+n2 = 20
+
+puts n1 - n2
+
+puts 3**2
+
+# pesquisar potenciacao
+
+puts 3.4.class # Imprime a classe da variavel
+
+puts "essa e uma".class
+
+# Arrays
+a = [3,5,7,2,1]
+
+# Metodos da array
+a.methods
+
+# ordena
+
+a.sort
+
+## Fim da aula 1
+
